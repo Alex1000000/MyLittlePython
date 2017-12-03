9766313137afbbb3cdd95f5c817e46e9fd427399
Alex1000000 <alexandra359m@gmail.com>
2017-12-03 23:29:39 +0300

    xml file added

--- task3_tab.py
+++ task3_tab.py
@@ -4,6 +4,7 @@ from tkinter import Text, END, INSERT,CURRENT,SEL_FIRST
 from tkinter.colorchooser import *
 from tkinter import filedialog
 
+import xml.etree.ElementTree as ET
 import matplotlib
 matplotlib.use('TkAgg')
 
@@ -25,6 +26,15 @@ def _quit():
     win.destroy()  # this is necessary on Windows to prevent
                     # Fatal Python Error: PyEval_RestoreThread: NULL tstate
 
+class CircleParamsListToSave:
+    listOfCircleParams = []
+    def addCircleToList(self,x,y,radius,color):
+        circleParams = {'x': x, 'y': y,'radius': radius ,'color': color}
+        self.listOfCircleParams.append(circleParams)
+    def clearList(self):
+        self.listOfCircleParams=[]
+    def addParamsList(self, newParamsList):
+        self.listOfCircleParams=newParamsList
 
 def Hello(event):
     print("Yet another hello world")
@@ -68,14 +78,19 @@ def onMouseClick(event):
         # print("mose CLICK LEFT")
         if currentColorFromCombo==0:
             circle1 = plt.Circle((x_mouse, y_mouse), scaleMy.get(), color='r')
+            currentCircleList.addCircleToList(x_mouse,y_mouse,scaleMy.get(),'r')
         elif currentColorFromCombo==1:
             circle1 = plt.Circle((x_mouse, y_mouse), scaleMy.get(), color='g')
+            currentCircleList.addCircleToList(x_mouse, y_mouse, scaleMy.get(), 'g')
         elif currentColorFromCombo==2:
             circle1 = plt.Circle((x_mouse, y_mouse), scaleMy.get(), color='b')
+            currentCircleList.addCircleToList(x_mouse, y_mouse, scaleMy.get(), 'b')
         elif currentColorFromCombo==3:
             circle1 = plt.Circle((x_mouse, y_mouse), scaleMy.get(), color='y')
+            currentCircleList.addCircleToList(x_mouse, y_mouse, scaleMy.get(), 'y')
         elif currentColorFromCombo==4:
             circle1 = plt.Circle((x_mouse, y_mouse), scaleMy.get(), color='m')
+            currentCircleList.addCircleToList(x_mouse, y_mouse, scaleMy.get(), 'm')
 
         a.add_artist(circle1)
         canvas.draw()
@@ -98,20 +113,55 @@ def onMouseMove(event):
     #print ('move')
 
 def file_save():
-    fi = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
+    #fi = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
+    # if fi is None: # asksaveasfile return `None` if dialog closed with "cancel".
+    #     return
+    root = ET.Element('circlesInThePlot')
+    ET.SubElement(root, 'settings',
+                  {'xlim': str(a.get_xbound()[1]), 'ylim': str(a.get_ybound()[1])})
+
+    for el in currentCircleList.listOfCircleParams:
+        print(el)
+        child = ET.SubElement(root, 'circle',
+                              {'x': str(el['x']), 'y': str(el['y']), 'radius': str(el['radius']), 'color': el['color']})
+    # ET.dump(root)
+    fi = filedialog.asksaveasfilename(filetypes=[("XML files", "*.xml")])
     if fi is None: # asksaveasfile return `None` if dialog closed with "cancel".
         return
-    text2save = "qwerty" # starts from `1.0`, not `0.0`
-    fi.write(text2save)
-    fi.close() # `()` was missing.
+    tree = ET.ElementTree(root)
+    tree.write(fi)
+    #text2save = "qwerty" # starts from `1.0`, not `0.0`
+    #//fi.write(text2save)
+    # fi.close() # `()` was missing.
 
 def file_open():
-    filename = filedialog.askopenfilename(initialdir="/", title="Select file",
-                                          filetypes=(("txt", "*.txt"), ("all files", "*.*")))
-    file = open(filename)
-    txt=file.read()
-    print(txt)
-    print("file open")
+    # filename = filedialog.askopenfilename(initialdir="/", title="Select file",
+    #                                       filetypes=(("txt", "*.txt"), ("all files", "*.*")))
+    # file = open(filename)
+    # txt=file.read()
+    # print(txt)
+    # print("file open")
+    a.cla()
+    filename = filedialog.askopenfilename(filetypes=[("XML files", "*.xml")])
+    tree = ET.parse(filename)
+    currentCircleList.clearList()
+    for node in tree.iter('settings'):
+        xlim = float(node.attrib.get('xlim'))
+        ylim = float(node.attrib.get('ylim'))
+        a.set_ylim([-ylim, ylim])
+        a.set_xlim([-xlim, xlim])
+        canvas.draw()
+
+
+    for node in tree.iter('circle'):
+        x = float(node.attrib.get('x'))
+        y = float(node.attrib.get('y'))
+        radius = float(node.attrib.get('radius'))
+        color = node.attrib.get('color')
+        currentCircleList.listOfCircleParams.append({ 'x': x, 'y': y, 'radius': radius, 'color': color})
+        circle1 = plt.Circle((x, y), radius, color=color)
+        a.add_artist(circle1)
+        canvas.draw()
 
 
 
@@ -173,7 +223,7 @@ def f_vect_scipy(z,t):
     r[11] = a3[1]
     return r
 
-def verlet(z,t,N_of_iterations):
+def verlet(z,dt,N_of_iterations):
     # z is a vector with 12 entries
     # ordered in blocks of 4:
     # x[k](t),x’[k](t),y[k](t),y’[k](t)
@@ -185,15 +235,14 @@ def verlet(z,t,N_of_iterations):
     #sun 1.98892 × 10^30   217000
     #moon 7.36 × 10^22     1020
     #earth 5.972 x 10^24   30000 м/с
-    m = [597200000, 7.36, 597.2]
+    # m = [597200000*(10**22), 7.36*(10**22), 597.2*(10**22)]
+    m = [1.98892  * (10 ** 30), 7.34767309*(10**22), 597.2 * (10 ** 22)]
     # relabel input vector z
     x1 = z[0]; u1 = z[1]; y1 = z[2]; v1 = z[3]
     x2 = z[4]; u2 = z[5]; y2 = z[6]; v2 = z[7]
     x3 = z[8]; u3 = z[9]; y3 = z[10]; v3 = z[11]
     # u and v are first derivatives of x and y
-    r[0] = u1; r[2] = v1
-    r[4] = u2; r[6] = v2
-    r[8] = u3; r[10] = v3
+
 
     r1=np.array([x1,y1])
     r2=np.array([x2,y2])
@@ -207,10 +256,10 @@ def verlet(z,t,N_of_iterations):
     dist13 = np.linalg.norm(r1 - r3)
     dist32 = np.linalg.norm(r3 - r2)
 
-    G = 10
-    a1 = G * (m[1] * (r2 - r1) / (dist12 ** 3) + m[2] * (r3 - r1) / (dist13 ** 3))/m[0]
-    a2 = G * (m[0] * (r1 - r2) / (dist12 ** 3) + m[2] * (r3 - r2) / (dist32 ** 3))/m[1]
-    a3 = G * (m[0] * (r1 - r3) / (dist13 ** 3) + m[1] * (r2 - r3) / (dist32 ** 3))/m[2]
+    G = 6.67408*(10**-11)# 10^-11
+    a1 = G * (m[1] * (r2 - r1) / (dist12 ** 3) + m[2] * (r3 - r1) / (dist13 ** 3))
+    a2 = G * (m[0] * (r1 - r2) / (dist12 ** 3) + m[2] * (r3 - r2) / (dist32 ** 3))
+    a3 = G * (m[0] * (r1 - r3) / (dist13 ** 3) + m[1] * (r2 - r3) / (dist32 ** 3))
 
     # N_of_iterations=10
 
@@ -223,30 +272,28 @@ def verlet(z,t,N_of_iterations):
     x3_n = np.zeros(N_of_iterations)
     y3_n = np.zeros(N_of_iterations)
 
-    delta_t=0.1
-
-    for i in range(0,N_of_iterations-1):
-        delta_t=t[i]
+    for i in range(0,N_of_iterations):
+        delta_t=dt#t[i]
 
         r1_next = r1 + vel1 * delta_t + 0.5 * a1*delta_t*delta_t
         r2_next = r2 + vel2 * delta_t + 0.5 * a2*delta_t*delta_t
         r3_next = r3 + vel3 * delta_t + 0.5 * a3*delta_t*delta_t
-        x1_n[i + 1]=r1_next[0]
-        y1_n[i + 1]=r1_next[1]
+        x1_n[i]=r1_next[0]
+        y1_n[i]=r1_next[1]
 
-        x2_n[i + 1]=r2_next[0]
-        y2_n[i + 1]=r2_next[1]
+        x2_n[i]=r2_next[0]
+        y2_n[i]=r2_next[1]
 
-        x3_n[i + 1]=r3_next[0]
-        y3_n[i + 1]=r3_next[1]
+        x3_n[i]=r3_next[0]
+        y3_n[i]=r3_next[1]
 
-        dist12 = np.linalg.norm(r1 - r2)
-        dist13 = np.linalg.norm(r1 - r3)
-        dist32 = np.linalg.norm(r3 - r2)
+        dist12 = np.linalg.norm(r1_next - r2_next)
+        dist13 = np.linalg.norm(r1_next - r3_next)
+        dist32 = np.linalg.norm(r3_next - r2_next)
 
-        a1_next = G * (m[1] * (r2 - r1) / (dist12 ** 3) + m[2] * (r3 - r1) / (dist13 ** 3))/m[0]
-        a2_next = G * (m[0] * (r1 - r2) / (dist12 ** 3) + m[2] * (r3 - r2) / (dist32 ** 3))/m[1]
-        a3_next = G * (m[0] * (r1 - r3) / (dist13 ** 3) + m[1] * (r2 - r3) / (dist32 ** 3))/m[2]
+        a1_next = G * (m[1] * (r2_next - r1_next) / (dist12 ** 3) + m[2] * (r3_next - r1_next) / (dist13 ** 3))
+        a2_next = G * (m[0] * (r1_next - r2_next) / (dist12 ** 3) + m[2] * (r3_next - r2_next) / (dist32 ** 3))
+        a3_next = G * (m[0] * (r1_next - r3_next) / (dist13 ** 3) + m[1] * (r2_next - r3_next) / (dist32 ** 3))
 
         vel1_next = vel1 + 0.5 * delta_t * (a1_next + a1)
         vel2_next = vel2 + 0.5 * delta_t * (a2_next + a2)
@@ -264,18 +311,18 @@ def verlet(z,t,N_of_iterations):
         a2=a2_next
         a3=a3_next
     # plotting the trajectories
-
-    # fig = plt.figure()
+    canvas.draw()
+    # fig = plt.figure("d")
     # a = fig.add_subplot(111)
+    a.plot(x1_n, y1_n,"r", x2_n, y2_n,"g", x3_n, y3_n,"b")
     xMax = max(max(x1_n), max(x2_n), max(x3_n))
     xMim = min(min(x1_n), min(x2_n), min(x3_n))
     yMax = max(max(y1_n), max(y2_n), max(y3_n))
     yMim = min(min(y1_n), min(y2_n), min(y3_n))
-    a.set_xlim([xMim - 100, xMax + 100])
-    a.set_ylim([yMim - 100, yMax + 100])
-    canvas.draw()
-    a.plot(x1_n, y1_n,"r", x2_n, y2_n,"g", x3_n, y3_n,"b")
+    a.set_xlim([xMim - 100000, xMax + 100000])
+    a.set_ylim([yMim - 100000, yMax + 100000])
     plt.show()
+
     return r
 
 
@@ -289,7 +336,7 @@ def scipy_n_body():
     ip2 = [0, -1499.80000]  #the moon
     ip3 = [0, -1496.00000]  #the earth 380.000  150.000.000
     # initial velocities
-    iv1=[1.0,1.0]#217000
+    iv1=[0.0,0.0]#217000
     iv3 = [72.0, 72.0]#1020
     iv2 = [0, 300.00]#30000
     # input for initial righthandside vector
@@ -330,21 +377,23 @@ def verlet_nBody():
     # forming a figure 8.
 
     # initial positions
-    ip1 = [0, 0]  #the sun
-    ip2 = [0, -1499.80000]  #the moon
-    ip3 = [0, -1496.00000]  #the earth 380.000  150.000.000
+    ip1 = [0, 0]  # ip1 = [1.496*10e11, 0]  #the sun
+    ip2 = [0, -1.496 * 1e11 - 384.467 * 1e6]  # [1022, -384467000]  #the moon
+    ip3 = [0, -1.496 * 1e11]  # [0, -149600000]  #the earth 380.000  150.000.000
     # initial velocities
-    iv1=[1.0,1.0]#217000
-    iv3 = [72.0, 72.0]#1020
-    iv2 = [0, 300.00]#30000
+    iv1 = [0.0, 0.0]  # 217000
+    iv2 = [1022 + 29.783 * 1e3, 0.0]  # [1022+29.783*1e3, 0.0]#-29.783*1e3]#1020
+    iv3 = [29.783 * 1e3, 0.0]  # 30000
     # input for initial righthandside vector
     initz = [ip1[0], iv1[0], ip1[1], iv1[1], \
-    ip2[0], iv2[0], ip2[1], iv2[1], \
-    ip3[0], iv3[0], ip3[1], iv3[1]]
-    T = 5#2 * sp.pi / 3;
-    n = 1000
-    tspan = sp.linspace(0, T, n + 1)
-    z_trajectories = verlet(initz, tspan,n)
+             ip2[0], iv2[0], ip2[1], iv2[1], \
+             ip3[0], iv3[0], ip3[1], iv3[1]]
+    T = 120  # 2 * sp.pi / 3;
+    n = 200000
+    # tspan = sp.linspace(0, T, n + 1)
+
+    z = verlet(initz, T, n)
+    return
 
 def n_body_solving():
     print(v.get())
@@ -408,7 +457,7 @@ tabControl.add(tab2, text='Model')      # Add the tab
 tabControl.pack(expand=1, fill="both")  # Pack to make visible
 
 
-
+currentCircleList= CircleParamsListToSave()
 f = Figure(figsize=(4, 4), dpi=100)
 a = f.add_subplot(111)
 t = arange(-100.0, 100.0, 1.0)
